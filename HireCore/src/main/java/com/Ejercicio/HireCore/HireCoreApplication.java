5181eb6616c8baccebe6b8bde05dd78112711d0d
leandroku <87959503+leandroku@users.noreply.github.com>
2026-09-13 12:32:28 -0500

    Primera carga

---
+++ HireCore/src/main/java/com/Ejercicio/HireCore/HireCoreApplication.java
@@ -0,0 +1,13 @@
+package com.Ejercicio.HireCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireCoreApplication.class, args);
+	}
+
+}
---
+++ HireCore/src/test/java/com/Ejercicio/HireCore/HireCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ejercicio.HireCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
